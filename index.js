0ed354e5a410bc72bba9308f619b19cc979dac7f
alx <alexanderkatmail@gmail.com>
2020-11-12 00:50:31 +0300

    feat: add put command

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const fs = require('fs-extra')
+const { Command } = require('commander')
+const program = new Command()
+
+const defaultTemplatesPath = './.splt/'
+
+program
+  .command('put <destinationPath> <templatePath> [name]')
+  .description('Copy component template')
+  .action((destinationPath, templatePath, name) => {
+    const fullTemplatePath = defaultTemplatesPath + templatePath
+    const templateName = fullTemplatePath.split('/').slice(-1)
+    
+    fs.copy(fullTemplatePath, `${destinationPath}/${name || templateName}`)
+  })
+
+program.parse(process.argv)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# SPLT: cli tool for fast boilerplate building
+
+###Basic usage
+
+Create template directory in the root of your project, name it `.splt`. Then put your templates in `.splt` with unique names. 
+
+###Available commands
+
+put
+usage:
+
+```bash
+splt put <destinationPath> <templatePath> [name]
+```
+
+example:
+
+```bash
+splt put ./src/components component my-compoent
+```
+
+run ```splt --help``` for information
